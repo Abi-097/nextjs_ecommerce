4a6952daf699a50931dceee790049e98546a4a13
Abi-097 <120549516+Abi-097@users.noreply.github.com>
2024-03-12 23:28:15 +0530

    space added

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <div className="min-h-screen flex flex-col">
           <Header />
+
           {children}
           <footer className="footer footer-center p-4 bg-base-300 text-base-content">
             <p>Copyright &#169; 2024 - All right reserved by Next Alexi</p>
