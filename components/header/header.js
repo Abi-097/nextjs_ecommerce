8228a71d5ff2efb19e3901daff11feb98a566f70
Abi-097 <120549516+Abi-097@users.noreply.github.com>
2024-03-14 00:06:15 +0530

    commit

--- components/header/header.js
+++ components/header/header.js
@@ -1,7 +1,27 @@
 import React from "react";
-
+import Link from "next/link";
+import Image from "next/image";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <nav>
+        <div className="navbar justify-between bg-base-300">
+          <Link href="/" className="btn btn-circle text-lg">
+            <Image src="/alexi.png" alt="alexi.png" height={24} width={24} />
+          </Link>
+          <ui className="flex">
+            <li>
+              <Link className="btn btn-ghost" href="/cart">
+                Cart
+              </Link>
+            </li>
+          </ui>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
+
+//btn-ghost <-d.ui
